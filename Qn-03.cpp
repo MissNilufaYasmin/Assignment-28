9a781f0f982cfa7e99ba5f29fce1df10870f5edd
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-07-21 20:01:13 +0530

    Create Qn-03.cpp

---
+++ Qn-03.cpp
@@ -0,0 +1,45 @@
+/*3. Overload subscript operator [] that will be useful when we want to check for an index
+out of bound.*/
+
+#include<iostream>
+using namespace std ;
+
+class Array
+{
+    int ary[10] ;
+    
+    
+    public:
+        void setData(int index_no , int value)
+        {
+            ary[index_no] = value ;
+        }
+        int operator[](int index_number)
+        {
+            if(index_number > 10 )
+            {
+                cout<<"Index number out of bound"<<endl ;
+                return 0 ;
+            }   
+            else
+                return ary[index_number] ;
+        }
+
+        
+} ;
+int main ()
+{
+    Array a1 ;
+    
+    //a1.setData(5,500) ;
+    //cout<<a1[5] ;
+
+    int i ;
+    for(i=0 ; i<10 ; i++)
+        a1.setData(i,i*5) ;
+    
+    for(i=0 ; i<10 ; i++)
+        cout<<a1[i]<<" " ;
+
+    return 0 ;
+}
